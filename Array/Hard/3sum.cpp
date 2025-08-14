f98ee4f4de6e6bb4cf360499549675548ee08df4
DivyanshuTewari <157007788+DivyanshuTewari@users.noreply.github.com>
2025-08-14 21:34:16 +0530

    Added optimized approach for 3Sum

--- Array/Hard/3sum.cpp
+++ Array/Hard/3sum.cpp
@@ -54,45 +54,47 @@ int main()
     */
 
     //Optimal solution without using set
-    sort(arr.begin(),arr.end());
     vector<vector<int>> result;
-    for(int i=0;i<n-3;i++)
+    sort(arr.begin(),arr.end());
+    for(int i=0;i<n-2;i++)
     {
-        int j = i+1;
-        int k = n-1;
         if(i>0 && arr[i]==arr[i-1])
         {
             continue;
         }
-
+        int j = i+1;
+        int k = n-1;
         while(j<k)
         {
             int sum = arr[i]+arr[j]+arr[k];
-            if(sum == 0)
-            {
-                result.push_back({arr[i],arr[j],arr[k]});
-                
-
-            }
             if(sum > 0)
             {
-               while(j<k && arr[k]==arr[k-1])
-               {
                 k--;
-               }
             }
-            if(sum < 0)
+            else if (sum<0)
             {
-                while(j<k && arr[j]==arr[j+1])
+                j++;
+            }
+            else
+            {
+                result.push_back({arr[i],arr[j],arr[k]});
+                j++;
+                k--;
+                while(j<k && arr[j]==arr[j-1])
                 {
                     j++;
                 }
+                while(j<k && arr[k]==arr[k+1])
+                {
+                    k--;
+                }
+
             }
-            j++;
-            k--;
         }
 
     }
+
+
     for(int i=0;i<result.size();i++)
     {
         for(int j=0;j<result[result.size()-1].size();j++)
