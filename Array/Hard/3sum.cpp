e0b890f9df0abf97ccf514e7134a53ed3f446cd6
DivyanshuTewari <157007788+DivyanshuTewari@users.noreply.github.com>
2025-08-14 07:32:34 +0530

    Solved 3sum with bruteforce

--- Array/Hard/3sum.cpp
+++ Array/Hard/3sum.cpp
@@ -3,17 +3,50 @@ using namespace std;
 int main()
 {
     int n ;
-    cin>>n;
+    cin>> n;
     vector<int> arr(n);
     for(int i=0;i<n;i++)
     {
         cin>>arr[i];
     }
-    int sum ;
-    cin>>sum;
 
-    //Brute force TC - O(n^3) Using hashing
     
-    
-    return 0;
+    set<vector<int>> st;
+    unordered_map<int,int> mpp;
+    for(int i=0;i<n;i++)
+    {
+        mpp.insert({arr[i],i});
+    }
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            int diff = - arr[i] - arr [j];
+            if(mpp.find(diff) != mpp.end())
+            {
+                if(mpp[diff] != i && mpp[diff] != j)
+                {
+                    vector<int> res;
+                    res.insert(res.end(),{arr[i],arr[j],diff});
+                    sort(res.begin(),res.end());
+                    st.insert(res);
+                }
+            }
+        }
+
+
+    }
+    vector<vector<int>> result;
+    for(auto it : st)
+    {
+        result.push_back(it);
+    }
+    for(int i=0;i<result.size();i++)
+    {
+        for(int j=0;j<result[result.size()-1].size();j++)
+        {
+            cout<<result[i][j]<<"   ";
+        }
+        cout<<endl;
+    }
 }
