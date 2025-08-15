2613fddb3eec591697adb90e8686956711aa162f
DivyanshuTewari <157007788+DivyanshuTewari@users.noreply.github.com>
2025-08-15 12:26:39 +0530

    Added Optimal approach for 4sum

--- Array/Hard/4sum.cpp
+++ Array/Hard/4sum.cpp
@@ -14,6 +14,7 @@ int main()
     vector<vector<int>> result;
     
     //Brute Force using set to avoid repeated values
+    /*
     
     set<vector<int>> st;
     for(int i = 0;i<n-3;i++)
@@ -40,6 +41,56 @@ int main()
     {
         result.push_back(it);
     }
+
+    */
+
+    //Optimal Approach 
+
+    sort(arr.begin(),arr.end());
+    for(int i = 0; i<n-3 ; i++)
+    {
+        if(i>0 && arr[i]==arr[i-1])
+        {
+            continue;
+        }
+        for(int j= i+1;j<n-2;j++)
+        {
+            if(j>i+1 && arr[j]==arr[j-1])
+            {
+                continue;
+            }
+            int k=j+1;
+            int l=n-1;
+            while(k<l)
+            {
+                int sum = arr[i]+arr[j]+arr[k]+arr[l];
+                if(sum>target)
+                {
+                    l--;
+                }
+                else if(sum<target)
+                {
+                    k++;
+                }
+                else
+                {
+                    result.push_back({arr[i],arr[j],arr[k],arr[l]});
+                    k++;
+                    l--;
+                    while(k<l && arr[k]==arr[k-1])
+                    {
+                        k++;
+                    }
+                    while(k<l && arr[l]==arr[l+1])
+                    {
+                        l--;
+                    }
+                }
+            }
+            
+        }
+    }
+
     for(int i=0;i<result.size();i++)
     {
         for(int j=0;j<result[result.size()-1].size();j++)
